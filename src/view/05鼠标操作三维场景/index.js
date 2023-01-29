b0202e1b2ccf24436cab2d9d2787b87ba8008b29
zwz <zouweizhe@guoxintianyu.com>
2023-01-29 14:54:29 +0800

    滚动效果

---
+++ src/view/05鼠标操作三维场景/index.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+// Three.js需要使用这个canvas标签来绘制，所以我们要先获取它然后传给three.js。
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({canvas});
+
+
+// 透视摄像机
+const fov = 75;
+const aspect = 2;  // 相机默认值
+const near = 0.1;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+// 场景
+const scene = new THREE.Scene();
+
+// 几何体
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+// 设置材质
+const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+// 创建一个网格(Mesh)对象，包含了
+// 几何体(Geometry)(物体的形状)
+// 材质(Material)(如何绘制物体，光滑还是平整，什么颜色，什么贴图等等)
+// 对象在场景中相对于他父对象的位置、朝向、和缩放。下面的代码中父对象即为场景对象。
+const cube = new THREE.Mesh(geometry, material);
+
+// 将网格添加到场景中
+scene.add(cube);
+
+// renderer.render(scene, camera);
+function render() {
+  renderer.render(scene,camera);//执行渲染操作
+}
+render();
+var controls = new OrbitControls( camera, renderer.domElement );
+controls.addEventListener('change', render);//监听鼠标、键盘事件
