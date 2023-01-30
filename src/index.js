feb0e1d8eb88636f8264dd5a60be4926a600387a
zwz <zouweizhe@guoxintianyu.com>
2023-01-30 10:12:02 +0800

    调整结构

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-// import './view/05鼠标操作三维场景/index'
-
-// import './view/03helloCube/helloCube'
-
-
-import './view/04第一个3D场景/index'
-
-
-// import './view/05鼠标操作三维场景/index'
+// import './view/01起步/index'
+// import './view/02helloCube/helloCube'
+// import './view/03第一个3D场景/index'
+import './view/04鼠标操作三维场景/index'
